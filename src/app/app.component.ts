845e27f53840d4cb221f8a6de847f212a5957e3e
meng  zhou <mengzhou44@mengs-MacBook-Pro.local>
2017-11-30 17:23:33 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,34 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AlertController } from 'ionic-angular';
+
+
 
 import { HomePage } from '../pages/home/home';
+
+//declare var Calculator: any;
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+
+
+  constructor(platform: Platform, statusBar: StatusBar,
+    splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      /* 
+            Calculator.add(3, 5, (result) => {
+              console.log('3+5=', result);
+            }) */
+
+
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
 }
-
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +10,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+
+  showAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Info',
+      subTitle: message,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  showLocation() {
+    console.log('showLocation()');
+    this.geolocation.getCurrentPosition().then((res) => {
+      var location = `latitude: ${res.coords.latitude}, longitude: ${res.coords.longitude}`
+      console.log('location: ', location);
+      this.showAlert(location);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private geolocation: Geolocation) {
 
   }
 
